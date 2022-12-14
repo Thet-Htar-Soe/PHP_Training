58d5860da6d080c5e15b2199e4303c6d0ee64c9f
thethtarsoe <thethtarsoe2761@gmail.com>
2022-12-14 08:01:01 +0630

    Tutorial04 review fixed

--- Tutorial_04/index.php
+++ Tutorial_04/index.php
@@ -11,38 +11,36 @@
 </head>
 
 <body>
-    <h1>Tutorial_04</h1>
-    <form action="" method="POST">
-        <input type="email" name="email" placeholder="Enter Your Email" value=<?php if (isset($_POST['login']) && isset($_POST['email'])) {
-        echo $_POST['email'];
-        } ?>>
-        <?php
-        if (isset($_POST['login']) && empty($_POST["email"])) {
-            echo "<span style='display:block;color:red'>Please Enter Your Email!!!</span>";
-        }
-        ?>
-        <br>
-        <input type="password" name="password" placeholder="Enter Your Password">
-        <?php
-        if (isset($_POST['login']) && empty($_POST["password"])) {
-            echo "<span style='display:block;color:red'>Please Enter Your Password!!!</span>";
-        }
-        ?>
-        <br>
-        <input type="submit" name="login" value="Login">
-    </form>
-
     <?php
+    $emptyEmail = "";
+    $emptyPsw = "";
+    $getEmail = "";
+    $emailErr = "";
     session_start();
-    if (isset($_SESSION['email'])) {
+    if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
         if (($_SESSION['email']) == "thethtarsoe2761@gmail.com" && $_SESSION['password'] == "123456") {
             header('Location:dashboard.php');
         }
     }
-
     if (isset($_POST['login'])) {
+
+        if ($_POST["email"] == null || $_POST["password"] == null) {
+            if ($_POST["email"] == null) {
+                $emptyEmail = "Please Enter Your Email!!!";
+            }
+            if ($_POST["password"] == null) {
+                $emptyPsw = "Please Enter Your Password!!!";
+            }
+        }
+
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format!!!";
+        }
+
+        $getEmail = $_POST["email"];
         $_SESSION['email'] =  $_POST['email'];
         $_SESSION['password'] =  $_POST['password'];
+
         if (($_SESSION['email']) == "thethtarsoe2761@gmail.com" && $_SESSION['password'] == "123456") {
             header('Location:dashboard.php');
         } elseif (!empty($_POST['email']) && !empty($_POST['password'])) {
@@ -51,11 +49,23 @@
             } elseif (($_SESSION['email']) !== "thethtarsoe2761@gmail.com" && $_SESSION['password'] == "123456") {
                 echo "<h2>Wrong Email!!!</h2>";
             } else {
-                echo "<h2>Please Try Again!!!</h2>"; 
+                echo "<h2>Email And Password does not match!!!</h2>";
             }
         }
     }
     ?>
+    <h1>Tutorial_04</h1>
+    <form action="" method="POST">
+        <input type="text" name="email" placeholder="Enter Your Email" value=<?php echo $getEmail; ?>>
+        <span style='display:block;color:red'><?php echo $emptyEmail;
+        echo  $emailErr; ?></span>
+        <br>
+        <input type="password" name="password" placeholder="Enter Your Password">
+        <span style='display:block;color:red'><?php echo $emptyPsw; ?></span>
+        <br>
+        <input type="submit" name="login" value="Login">
+    </form>
+
 </body>
 
 </html>
