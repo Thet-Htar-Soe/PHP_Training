036be22edcb2feead585c2a6ba2e29885a66e396
thethtarsoe <thethtarsoe2761@gmail.com>
2022-12-16 15:50:22 +0630

    Review Fixed Error Msg

--- Tutorial_04/index.php
+++ Tutorial_04/index.php
@@ -16,6 +16,9 @@
     $emptyPsw = "";
     $getEmail = "";
     $emailErr = "";
+    $wrongPsw = "";
+    $wrongEmail = "";
+    $wrongMailPsw = "";
     session_start();
     if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
         if (($_SESSION['email']) == "thethtarsoe2761@gmail.com" && $_SESSION['password'] == "123456") {
@@ -33,9 +36,7 @@
             }
         }
 
-        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-            $emailErr = "Invalid email format!!!";
-        }
+
 
         $getEmail = $_POST["email"];
         $_SESSION['email'] =  $_POST['email'];
@@ -45,11 +46,20 @@
             header('Location:dashboard.php');
         } elseif (!empty($_POST['email']) && !empty($_POST['password'])) {
             if (($_SESSION['email']) == "thethtarsoe2761@gmail.com" && $_SESSION['password'] !== "123456") {
-                echo "<h2>Wrong Password!!!</h2>";
-            } elseif (($_SESSION['email']) !== "thethtarsoe2761@gmail.com" && $_SESSION['password'] == "123456") {
-                echo "<h2>Wrong Email!!!</h2>";
+                $wrongPsw = "Wrong Password!!!";
+            } elseif (($_SESSION['email']) !== "thethtarsoe2761@gmail.com" && $_SESSION['password'] == "123456" && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                $wrongEmail = "Wrong Email!!!";
+            } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) && $_SESSION['password'] == "123456") {
+                $emailErr = "Invalid email format!!!";
+            } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) && $_SESSION['password'] !== "123456") {
+                $emailErr = "Invalid email format!!!";
+                $wrongPsw = "Wrong Password!!!";
             } else {
-                echo "<h2>Email And Password does not match!!!</h2>";
+                $wrongMailPsw =  "Email And Password does not match!!!";
+            }
+        } elseif (!empty($_POST['email'])) {
+            if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Invalid email format!!!";
             }
         }
     }
@@ -57,14 +67,18 @@
     <h1>Tutorial_04</h1>
     <form action="" method="POST">
         <input type="text" name="email" placeholder="Enter Your Email" value=<?php echo $getEmail; ?>>
-        <span style='display:block;color:red'><?php echo $emptyEmail;
-        echo  $emailErr; ?></span>
+        <h2><?php echo $emptyEmail; ?></h2>
+        <h2><?php echo $emailErr; ?></h2>
+        <h2><?php echo $wrongEmail ?></h2>
         <br>
         <input type="password" name="password" placeholder="Enter Your Password">
-        <span style='display:block;color:red'><?php echo $emptyPsw; ?></span>
+        <h2><?php echo $emptyPsw; ?></h2>
+        <h2><?php echo $wrongPsw; ?></h2>
         <br>
         <input type="submit" name="login" value="Login">
+        <br>
     </form>
+    <h3><?php echo $wrongMailPsw; ?></h3>
 
 </body>
 
